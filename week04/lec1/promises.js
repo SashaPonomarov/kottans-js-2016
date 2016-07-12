dd110a73966f9560a10babf33db6c2ac179810bd
Sasha Ponomarov <randirsbox@gmail.com>
2016-07-12 16:31:42 +0300

    Add check if input is iterable

--- week04/lec1/promises.js
+++ week04/lec1/promises.js
@@ -1,22 +1,35 @@
 'use strict'
 
+const isIterable = function (obj) {
+  if (obj == null) {
+    return false;
+  }
+  return typeof obj[Symbol.iterator] === 'function';
+}
+
 class MyPromise extends Promise {
+
     static map(input, mapper) {
         let results = []
         let waiting = 0
         return new Promise(function(resolve, reject) {
-            Promise.resolve(input).then(function(data) {
-                for (let i = 0; i < data.length; i++) 
-                {
-                    waiting++
-                    Promise.resolve(data[i]).then(function(element) {
-                        Promise.resolve(mapper(element)).then(function(result) {
-                            results.push(result)
-                            if (--waiting == 0) {
-                                resolve(results)
-                            }
-                        }, reject)                
-                    }, reject)
+            Promise.resolve(input).then(function(data) { //in case input is promised
+                if (isIterable(data)) {
+                    for (let i = 0; i < data.length; i++) 
+                    {
+                        waiting++
+                        Promise.resolve(data[i]).then(function(element) { //in case separate elements of input are promised
+                            Promise.resolve(mapper(element)).then(function(result) { //in case mapper returns Promise
+                                results.push(result)
+                                if (--waiting == 0) {
+                                    resolve(results)
+                                }
+                            }, reject)                
+                        }, reject)
+                    }
+                }
+                else {
+                    reject(new TypeError('input is not iterable'))
                 }
             }, reject)
         })
