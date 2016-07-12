315e670776749b5c78bbedb2c081dc461932b43d
Sasha Ponomarov <randirsbox@gmail.com>
2016-07-12 16:01:51 +0300

    Put map function into MyPromise class; resolve input Promise

--- week04/lec1/promises.js
+++ week04/lec1/promises.js
@@ -1,21 +1,24 @@
 'use strict'
 
-function map(input, mapper) {
-	let results = []
-	let waiting = 0
-	return new Promise(function(resolve, reject) {
-		for (let i = 0; i < input.length; i++) 
-		{
-			waiting++
-			Promise.resolve(input[i]).then(function(element) {
-					Promise.resolve(mapper(element)).then(function(result) {
-						results.push(result)
-						if (--waiting == 0) {
-							resolve(results)
-						}
-					}, reject)				
-				}, reject)
-		}
-	})
-
+class MyPromise extends Promise {
+    static map(input, mapper) {
+        let results = []
+        let waiting = 0
+        return new Promise(function(resolve, reject) {
+            Promise.resolve(input).then(function(data) {
+                for (let i = 0; i < data.length; i++) 
+                {
+                    waiting++
+                    Promise.resolve(data[i]).then(function(element) {
+                        Promise.resolve(mapper(element)).then(function(result) {
+                            results.push(result)
+                            if (--waiting == 0) {
+                                resolve(results)
+                            }
+                        }, reject)                
+                    }, reject)
+                }
+            }, reject)
+        })
+    }
 }
